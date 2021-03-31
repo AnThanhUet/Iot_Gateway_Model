0d83a0ba4b3ace1554aaa62daecb6edaa8da99b2
AnThanh <theanuet@gmail.com>
2021-03-31 15:20:15 +0700

    do an

--- main/sub_node_pub_thing/sub_node_pub_thing_save_database.py
+++ main/sub_node_pub_thing/sub_node_pub_thing_save_database.py
@@ -2,11 +2,11 @@ import paho.mqtt.client as mqtt
 import json
 import random
 from time import sleep
-from publish_thingsboard import pub
+#from publish_thingsboard import pub
 from save_database import save
 
 # MQTT setting
-MQTT_Broker = "192.168.137.136"
+MQTT_Broker = "192.168.137.68"
 MQTT_Port = 1883
 Keep_Alive_Interval = 7200
 MQTT_Topic = 'hello'
@@ -22,7 +22,7 @@ def on_connect(client, userdata, flags, rc):
 # Callback on_message server
 def on_message(client, userdata, msg):
     print("Subscribe Message Recieved: "+msg.payload.decode())
-    pub(msg.payload)
+   # pub(msg.payload)
     #save(msg.payload.decode()) #save database
     #mosquitto_pub -d -t hello -m "{\"Area\":\"xuanthuy\",\"ID\":1,\"Temperature\":4,\"Humidity\":45}"
 
